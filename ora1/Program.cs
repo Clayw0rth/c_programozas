b976e26bf8385534bb0ca98900f81c8027210abd
Király Mátyás <100215642+Clayw0rth@users.noreply.github.com>
2022-09-21 22:04:08 +0200

    Add files via upload

---
+++ ora1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ora1
+{
+    class Object {
+        static int max, min, num;
+
+        public Object(){
+            num = 10;
+            max++;
+            min++;
+            Console.WriteLine(max);
+        }
+
+        ~Object() {
+            max--;
+        }
+
+        public int getMax() {
+            return max;
+        }
+
+        public int getMin()
+        {
+            return min;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            for (int i = 0; i < 500000; i++) {
+                Object myObj = new Object();
+                if (myObj.getMin() > myObj.getMax()) {
+                    break;
+                }
+            }
+        }
+    }
+}
